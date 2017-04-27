eb290819f1c0b2a0d17fb68449e9e8a6e0919471
Kees Kluskens <kees@webduck.nl>
2017-04-27 16:54:43 +0200

    refactor examples to actually React on changes
    *stupid joke*

--- stories/index.js
+++ stories/index.js
@@ -1,78 +1,104 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { storiesOf, action } from '@kadira/storybook';
 import Multipick from '../src';
 import CenterDecorator from './CenterDecorator';
 
+const SOME_DATA = [
+    {
+        value: '1',
+        label: 'Dinosaurus',
+    },
+    {
+        value: '2',
+        label: 'Butterflies',
+    },
+    {
+        value: '3',
+        label: 'Giraffes',
+    },
+];
+
+const MORE_DATA = [
+    ...SOME_DATA,
+    {
+        value: '4',
+        label: 'Roger',
+    },
+    {
+        value: '5',
+        label: 'Monkeys',
+    },
+];
+
 storiesOf('Multipick', module)
     .addDecorator(CenterDecorator)
     .add('with some selections', () => {
-        const DATA = [
-            {
-                value: '1',
-                label: 'Dinosaurus',
-            },
-            {
-                value: '2',
-                label: 'Butterflies',
-            },
-            {
-                value: '3',
-                label: 'Giraffes',
-            },
-        ];
-        return (
-            <Multipick
-                data={DATA}
-                values={['1', '3']}
-                onChange={action('change')}
-            />
-        );
+        class MyComponent extends Component {
+            state = {
+                values: ['1', '3'],
+            };
+
+            handleChange = values => {
+                this.setState({ values });
+                action('change');
+            };
+
+            render() {
+                return (
+                    <Multipick
+                        data={SOME_DATA}
+                        values={this.state.values}
+                        onChange={this.handleChange}
+                    />
+                );
+            }
+        }
+        return <MyComponent />;
     })
     .add('without any selections', () => {
-        const DATA = [
-            {
-                value: '1',
-                label: 'Dinosaurus',
-            },
-            {
-                value: '2',
-                label: 'Butterflies',
-            },
-            {
-                value: '3',
-                label: 'Giraffes',
-            },
-        ];
-        return <Multipick data={DATA} onChange={action('change')} />;
+        class MyComponent extends Component {
+            state = {
+                values: [],
+            };
+
+            handleChange = values => {
+                this.setState({ values });
+                action('change');
+            };
+
+            render() {
+                return (
+                    <Multipick
+                        data={SOME_DATA}
+                        values={this.state.values}
+                        onChange={this.handleChange}
+                    />
+                );
+            }
+        }
+        return <MyComponent />;
     })
     .add('with searchbar', () => {
-        const DATA = [
-            {
-                value: '1',
-                label: 'Dinosaurus',
-            },
-            {
-                value: '2',
-                label: 'Butterflies',
-            },
-            {
-                value: '3',
-                label: 'Giraffes',
-            },
-            {
-                value: '4',
-                label: 'Roger',
-            },
-            {
-                value: '5',
-                label: 'Monkeys',
-            },
-        ];
-        return (
-            <Multipick
-                data={DATA}
-                onChange={action('change')}
-                searchAppearsAfterCount={5}
-            />
-        );
+        class MyComponent extends Component {
+            state = {
+                values: [],
+            };
+
+            handleChange = values => {
+                this.setState({ values });
+                action('change');
+            };
+
+            render() {
+                return (
+                    <Multipick
+                        data={MORE_DATA}
+                        values={this.state.values}
+                        searchAppearsAfterCount={5}
+                        onChange={this.handleChange}
+                    />
+                );
+            }
+        }
+        return <MyComponent />;
     });
